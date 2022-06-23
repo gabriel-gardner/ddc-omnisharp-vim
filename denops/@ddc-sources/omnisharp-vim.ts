b51ff1c61a1eeccce477103d84d5fe7f87ce48d8
Gabriel Gardner <gabriel.gardner@scherzo.games>
2022-06-23 13:20:28 -0400

    Added ddc-ultisnips to test whether this is noticed

--- denops/@ddc-sources/omnisharp-vim.ts
+++ denops/@ddc-sources/omnisharp-vim.ts
@@ -5,8 +5,16 @@ import {
 import {
   GatherArguments,
 } from "https://deno.land/x/ddc_vim@v2.2.0/base/source.ts";
+import { Denops, vars } from "https://deno.land/x/ddc_vim@v2.2.0/deps.ts";
 
 type Params = Record<never, never>;
+
+export type Snippets = {
+  [word: string]: {
+    location: string;
+    description: string;
+  };
+};
 
 export class Source extends BaseSource<Params> {
   private previousLhs: string = "";
@@ -15,6 +23,26 @@ export class Source extends BaseSource<Params> {
     args: GatherArguments<Params>,
   ): Promise<Item[]> {
       const currentInput = args.context.input;
+    if (await vars.g.get(args.denops, "did_plugin_ultisnips") == null) {
+      return [];
+    }
+
+    const snippets = await args.denops.call(
+      "UltiSnips#SnippetsInCurrentScope",
+      1,
+    ) as {
+      [trigger: string]: string;
+    };
+    const info = await vars.g.get(
+      args.denops,
+      "current_ulti_dict_info",
+    ) as Snippets;
+    return Object.keys(snippets).map((trigger) => ({
+      word: trigger,
+      menu: snippets[trigger],
+      user_data: JSON.stringify({ "ultisnips": info[trigger] }),
+    }));
+
 
         
     return Object.keys(["abra", "cadabra"]).map((trigger) => ({
