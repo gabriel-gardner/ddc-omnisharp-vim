fb7c45ab7caaaa2e270f79d938867e2dac95f18b
Gabriel Gardner <gabriel.gardner@scherzo.games>
2022-06-23 11:55:26 -0400

    Getting typescript working

---
+++ denops/@ddc-sources/omnisharp-vim.ts
@@ -0,0 +1,64 @@
+import {
+  BaseSource,
+  Item,
+} from "https://deno.land/x/ddc_vim@v2.2.0/types.ts";
+import {
+  GatherArguments,
+} from "https://deno.land/x/ddc_vim@v2.2.0/base/source.ts";
+
+type Params = Record<never, never>;
+
+export class Source extends BaseSource<Params> {
+  private previousLhs: string = "";
+  private previousPartial: string = "";
+  async gather(
+    args: GatherArguments<Params>,
+  ): Promise<Item[]> {
+      const currentInput = args.context.input;
+
+      const [lhs, partial] = this.parseInput(currentInput);
+
+      if (!lhs) {
+            // return null
+            return [];
+        }
+
+      if (lhs != this.previousLhs || partial.startsWith(this.previousPartial))
+      {
+            this.previousLhs = lhs;
+            this.previousPartial = partial;
+            const results = await args.denops.call(
+              "Omnisharp#actions#complete#Get",
+              lhs,
+              partial,
+            ) as {
+              [trigger: string]: string;
+            };
+            return [];
+          //self.previousLhs = lhs
+          //self.previousPartial = partial
+          //self.vim.call('deoplete#source#omnisharp#sendRequest', lhs, partial)
+          //return []
+      }
+      return [];
+  }
+
+  private parseInput(
+    input: string,
+  ): [string, string] {
+        const match = input.match(/r"^(.*\W)(\w*)$"/);
+
+        if (match != null) {
+            return [match[0], match[1]];
+        }        
+        return ["", ""];
+    }
+
+
+  params(): Params {
+    return {
+      kindLabels: {},
+    };
+  }
+}
+
